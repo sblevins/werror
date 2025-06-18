bc5347ff87c96c2650b9460c29ab6c819ef43537
sblevins <15042104+sblevins@users.noreply.github.com>
2025-06-18 12:09:40 +1100

    Update werror.go

--- werror.go
+++ werror.go
@@ -3,37 +3,45 @@ package werror
 import (
 	"errors"
 	"fmt"
-	"runtime/debug"
+	"runtime"
 	"strings"
 )
 
-const myName = "werror.go"
+// getCallerInfo returns the package, function, and line of the first external caller
+func getCallerInfo() (pkg string, function string, line int) {
+	// skip=0 is getCallerInfo
+	// skip=1 is Wrap or New
+	// skip=2 is the actual caller
+	pc := make([]uintptr, 10)
+	n := runtime.Callers(3, pc) // skip 3 to get the first external caller
+	if n == 0 {
+		return "unknown", "unknown", 0
+	}
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
 
-func getCallerInfo() (pkg string, function string, line string) {
-	stack := string(debug.Stack())
-	_, stack, _ = strings.Cut(stack, "[running]:\n") //cut the calling goroutine
-	_, stack, _ = strings.Cut(stack, myName)         //cut the call made by this function
-	_, stack, _ = strings.Cut(stack, myName)         //cut the call made by Wrap/New
-	_, stack, _ = strings.Cut(stack, "\n")           //cut the line number and hex data at the end
-	caller, stack, _ := strings.Cut(stack, "\n")     //cut the function call from the stack data
-	_, stack, _ = strings.Cut(stack, ":")            //cut the stack from the line number and hex data
-	line, _, _ = strings.Cut(stack, " ")             //cut the line number from the hex data
-	line, _, _ = strings.Cut(line, "\n")             //if there was no extra hex data we need to cut here to recover
-	pkg, function, _ = strings.Cut(caller, ".")      //split the calling package from the calling function
+	funcName := frame.Function // e.g., "main.doSomething"
+	split := strings.Split(funcName, ".")
+	if len(split) >= 2 {
+		pkg = strings.Join(split[:len(split)-1], ".")
+		function = split[len(split)-1]
+	} else {
+		pkg = "unknown"
+		function = funcName
+	}
 
-	return pkg, function, line
+	return pkg, function, frame.Line
 }
 
-//wraps errors to include information about the caller and a specified message
 func Wrap(msg string, err error) error {
 	pkg, function, line := getCallerInfo()
-	infoMsg := "error encountered in pkg: " + pkg + ", caller: " + function + ":" + line + ", msg: " + msg
-	return fmt.Errorf(infoMsg+"\n >%w", err)
+	infoMsg := fmt.Sprintf("error encountered in pkg: %s, caller: %s:%d, msg: %s", pkg, function, line, msg)
+	return fmt.Errorf("%s\n > %w", infoMsg, err)
 }
 
-//creates a new error with information about the caller and a specified message
 func New(msg string) error {
 	pkg, function, line := getCallerInfo()
-	infoMsg := "error raised in pkg: " + pkg + ", caller: " + function + ":" + line + ", msg: " + msg
+	infoMsg := fmt.Sprintf("error raised in pkg: %s, caller: %s:%d, msg: %s", pkg, function, line, msg)
 	return errors.New(infoMsg)
 }
+
